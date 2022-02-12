0f5f5c1e1f490c8bd695aa0986e4f44e287722a4
FranFrioni <francescofrioni@gmail.com>
2022-02-12 12:51:14 -0300

    Sort de data agregado y visuales

--- script.js
+++ script.js
@@ -19,16 +19,23 @@ function isEmptyList(){
 }
 
 function refreshList(){
-    let list = "<div class='table-responsive'><table class='table'><thead><tr class='table-dark'><th scope='col'></th><th scope='col'>Name</th><th scope='col'>Amount</th><th scope='col'>Date</th><th scope='col'></th></tr></thead><tbody>";
-    for (let i = 0; i < itemList.length; i++) {
-        list += "<tr class='table-light'><th scope='row' class='border-end border-dark'>" + (i+1) + "</th><td id='name" + i + "' class='border border-dark'>Name</td><td id='amount" + i + "'class='border border-dark'>Amount</td><td id='date" + i + "'class='border border-dark'>Date</td>" +
-        "<td class='border-bottom border-dark'><button class='btn btn-outline-danger' type='button' onclick='removeExpense(" + i + ")'>Delete</button></td></tr>";
+    let list = "<div class='table-responsive'><table class='table'><thead><tr class='table-dark'>" + 
+    "<th scope='col' class='text-center'>Id<button type='button' class='btn btn-dark p-0' onclick='sortItemsByIdAscendant()'>(↓)</button><button type='button' class='btn btn-dark p-0' onclick='sortItemsByIdDescendent()'>(↑)</button></th>" +
+    "<th scope='col' class='text-center'>Name<button type='button' class='btn btn-dark p-0' onclick='sortItemsByNameAscendant()'>(↓)</button><button type='button' class='btn btn-dark p-0' onclick='sortItemsByNameDescendent()'>(↑)</button></th>" +
+    "<th scope='col' class='text-center'>Amount<button type='button' class='btn btn-dark p-0' onclick='sortItemsByAmountAscendant()'>(↓)</button><button type='button' class='btn btn-dark p-0' onclick='sortItemsByAmountDescendent()'>(↑)</button></th>" + 
+    "<th scope='col' class='text-center'>Date<button type='button' class='btn btn-dark p-0' onclick='sortItemsByDateAscendant()'>(↓)</button><button type='button' class='btn btn-dark p-0' onclick='sortItemsByDateDescendent()'>(↑)</button></th>" +
+    "<th scope='col' class='text-center'></th></tr></thead><tbody>";
+    for (let i = 1; i <= itemList.length; i++) {
+        list += "<tr class='table-light text-center'><th scope='row' class='border-end border-dark' id='id" + i + "'></th><td id='name" + i + "' class='border border-dark'>Name</td><td id='amount" + i + "'class='border border-dark'>Amount</td><td id='date" + i + "'class='border border-dark'>Date</td>" +
+        "<td class='border-bottom border-dark text-center'><button class='btn btn-outline-danger' type='button' onclick='removeExpense(" + itemList[i-1].id + ")'>Delete</button></td></tr>";
     }
-    document.getElementById("list").innerHTML = list + "</tbody><tfoot><tr class='table-secondary fs-4'><td></td><td colspan='3'>Total</td><td>" + totalAmount() + "</td></tr></tfoot></table></div>";
-    for (let i = 0; i < itemList.length; i++) {
-        document.getElementById("name"+i).textContent = itemList[i].name;
-        document.getElementById("amount"+i).textContent = itemList[i].amount;
-        document.getElementById("date"+i).textContent = itemList[i].date;
+    document.getElementById("list").innerHTML = list + "</tbody><tfoot><tr class='table-secondary fs-4'><td class='fw-bold text-center'>Total</td><td class='fw-bold text-center' colspan='4'>" + "$" + totalAmount() + "</td></tr></tfoot></table></div>";
+    for (let i = 1; i <= itemList.length; i++) {
+        document.getElementById("id"+i).textContent = itemList[i-1].id;
+        document.getElementById("name"+i).textContent = itemList[i-1].name;
+        document.getElementById("amount"+i).textContent = "$" + itemList[i-1].amount;
+        let date = itemList[i-1].date.split("-");
+        document.getElementById("date"+i).textContent = date[2] + "/" + date[1] + "/" + date[0];
     }
     refreshCounter();
 }
@@ -50,7 +57,7 @@ function addExpense(){
         document.getElementById("inputAddName").value = "";
         document.getElementById("inputAddAmount").value = "";
         document.getElementById("inputAddDate").value = "";
-        itemList.push({name: expenseName, amount: parseInt(expenseAmount), date: expenseDate});
+        itemList.push({id: (itemList.length + 1), name: expenseName, amount: parseInt(expenseAmount), date: expenseDate});
         localStorage.itemList = JSON.stringify(itemList);
         document.getElementById("alert").classList.add("d-none");
         refreshList();
@@ -59,8 +66,19 @@ function addExpense(){
     }
 }
 
-function removeExpense(index){
+function removeExpense(id){
+    let index;
+    for (let i = 0; i < itemList.length; i++) {
+        if (id == itemList[i].id){
+            index = i;
+        }
+    }
     itemList.splice(index, 1);
+    itemList.forEach(item => {
+        if (item.id > id){
+           item.id--; 
+        } 
+    });
     localStorage.itemList = JSON.stringify(itemList);
     refreshList();
     isEmptyList();
@@ -72,4 +90,68 @@ function totalAmount(){
         totalAmount += item.amount;
     });
     return totalAmount;
+}
+
+function sortItemsByIdAscendant(){
+    itemList.sort(function(a, b) {
+        return a.id - b.id;
+    });
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByIdDescendent(){
+    itemList.sort(function(a, b) {
+        return b.id - a.id;
+    });
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByNameAscendant(){
+    itemList.sort(function (a, b) {
+        return ('' + a.name).localeCompare(b.name);
+    })
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByNameDescendent(){
+    itemList.sort(function (a, b) {
+        return ('' + b.name).localeCompare(a.name);
+    })
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByAmountAscendant(){
+    itemList.sort(function(a, b) {
+        return a.amount - b.amount;
+    });
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByAmountDescendent(){
+    itemList.sort(function(a, b) {
+        return b.amount - a.amount;
+    });
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByDateAscendant(){
+    itemList.sort(function(a,b){
+        return new Date(a.date) - new Date(b.date);
+    });
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
+}
+
+function sortItemsByDateDescendent(){
+    itemList.sort(function(a,b){
+        return new Date(b.date) - new Date(a.date);
+    });
+    refreshList();
+    localStorage.itemList = JSON.stringify(itemList);
 }
